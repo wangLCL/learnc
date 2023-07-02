de0588ab71acbedbe6d9678397dc0aecfd5b2c1e
kangwang <2818815189@qq.com>
2023-07-02 23:02:24 +0800

    add note

---
+++ code/basePart/threaddemo/threaddemo/threaddemo.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <thread>
+#include <windows.h>
+using namespace std;
+void func() {
+    for (int i = 0; i <= 10; i++) {
+        cout << "这是一句话！" << endl;
+        Sleep(1000);
+    }
+}
+
+int main()
+{
+    thread* t1 = new thread(func);
+    for (int i = 0; i <= 10; i++) {
+        std::cout << "Hello World!\n";
+        Sleep(1000);
+    }
+    t1->join();
+
+    thread t2(func);
+}
