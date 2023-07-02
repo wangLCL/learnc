0f59507fdfe2e8843561c410669c4a0342dedc6b
kangwang <2818815189@qq.com>
2023-07-02 21:38:12 +0800

    add note

---
+++ code/basePart/zimianliang/zimianliang/zimianliang.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+using namespace std;
+
+typedef long int lint;
+
+int main()
+{
+    string path = "c:\\user";
+    string path1 = R"aaa(c:\user;)aaa";
+    cout << "Hello World!\n";
+}
