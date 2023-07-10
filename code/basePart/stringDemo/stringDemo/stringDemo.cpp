24789121ca4e6297628743a60dc5d73b3ad2b971
kangwang <2818815189@qq.com>
2023-07-10 08:25:51 +0800

    string代码

---
+++ code/basePart/stringDemo/stringDemo/stringDemo.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+    string str;//创建一个长度为0的string对象
+    cout << str.npos << endl;
+    //得到当前容量，可以存放字符的总数
+    //15 31
+    cout<< "str.capacity():" << str.capacity() << endl;
+    cout << "s.size()" << str.size() << endl;//返回数据的大小
+    cout << "" << (void*)str.c_str() << endl;
+
+    //拷贝构造函数
+    string ss = str;
+    string s1(str);
+
+}
